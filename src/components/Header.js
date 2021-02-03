c661647231175d2ee54a229c5e8acd36f168de20
Joshua Miller <joshsmiller1022@gmail.com>
2021-02-03 13:02:38 -0700

    Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return <header className="header">
         <div>
-            <h1>Screen Time</h1>
+            <h1>SCREEN TIME</h1>
         </div>
     </header>
 }
