e811629c2fa19cbce0309e2ff526d82099b06d0b
Joshua Miller <joshsmiller1022@gmail.com>
2021-02-03 10:02:30 -0700

    DONE

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -5,12 +5,14 @@ const Editor = props => {
     return <div className="editor">
         <div>{name}</div>
         <div>{day}</div>
-        <div>{hours}</div>
-        <div className="buttons">
-            {/* <button className="addBtn" onClick={() => props.updateChild(id)}> + </button>
-            <button className="subBtn" onClick={() => props.updateChild(id)}> - </button> */}
-            <button className="addBtn" onClick={() => props.addHours(hours, id)}> + </button>
-            <button className="subBtn" onClick={() => props.subHours(hours, id)}> - </button>
+        <div className="hoursBox">
+            <div className={hours < 0 || hours > 24 ? "badData" : "goodData"}>{hours}</div>
+            <div className="buttons">
+                <button className="addBtn" onClick={() => props.addHours(hours, id)}> + </button>
+                <button className="subBtn" onClick={() => props.subHours(hours, id)}> - </button>
+            </div>
+        </div>
+        <div className="delete">
             <button onClick={() => props.deleteChild(id)}>Delete</button>
         </div>
     </div>
